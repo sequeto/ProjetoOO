18a7d8dd693c21c7fdb8ecfbd57432297c610695
sequeto <joaopedrosequeto@gmail.com>
2019-09-29 15:37:42 -0300

    Adicionando atributos e construtor da classe cliente

--- src/main/java/classes/Cliente.java
+++ src/main/java/classes/Cliente.java
@@ -4,11 +4,21 @@
  * and open the template in the editor.
  */
 package classes;
+import java.lang.*;
+import java.util.*;
 
 /**
  *
  * @author Sequeto
  */
 public class Cliente {
+    private String nome;
+    private String telefone;
+    private String cpf;
     
+    public Cliente(String nome, String cpf, String telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    } 
 }
